daeabee7b1726b77994f4ff8a93cbd608ca8b0ca
jamesaduke <jamesodwuor4@gmail.com>
2017-03-25 08:43:20 +0300

    add indentation and spacing changes

--- recursion/moreFractal.py
+++ recursion/moreFractal.py
@@ -1,38 +1,38 @@
-import pygame, math
+import pygame
+import math
 pygame.init()           # prepare the pygame module for use
 
 # Create a new surface and window.
 surface_size = 1024
-main_surface = pygame.display.set_mode((surface_size,surface_size))
+main_surface = pygame.display.set_mode((surface_size, surface_size))
 my_clock = pygame.time.Clock()
 
 
 def draw_tree(order, theta, sz, posn, heading, color=(0,0,0), depth=0):
+    trunk_ratio = 0.29       # How big is the trunk relative to whole tree?
+    trunk = sz * trunk_ratio   # length of trunk
+    delta_x = trunk * math.cos(heading)
+    delta_y = trunk * math.sin(heading)
+    (u, v) = posn
+    newpos = (u + delta_x, v + delta_y)
+    pygame.draw.line(main_surface, color, posn, newpos)
 
-   trunk_ratio = 0.29       # How big is the trunk relative to whole tree?
-   trunk = sz * trunk_ratio # length of trunk
-   delta_x = trunk * math.cos(heading)
-   delta_y = trunk * math.sin(heading)
-   (u, v) = posn
-   newpos = (u + delta_x, v + delta_y)
-   pygame.draw.line(main_surface, color, posn, newpos)
+    if order > 0:   # Draw another layer of subtrees
 
-   if order > 0:   # Draw another layer of subtrees
+        # These next six lines are a simple hack to make the two major halves
+        # of the recursion different colors. Fiddle here to change colors
+        # at other depths, or when depth is even, or odd, etc.
+        if depth == 0:
+            color1 = (255, 0, 0)
+            color2 = (0, 0, 255)
+        else:
+            color1 = color
+            color2 = color
 
-      # These next six lines are a simple hack to make the two major halves
-      # of the recursion different colors. Fiddle here to change colors
-      # at other depths, or when depth is even, or odd, etc.
-      if depth == 0:
-          color1 = (255, 0, 0)
-          color2 = (0, 0, 255)
-      else:
-          color1 = color
-          color2 = color
-
-      # make the recursive calls to draw the two subtrees
-      newsz = sz*(1 - trunk_ratio)
-      draw_tree(order-1, theta, newsz, newpos, heading-theta, color1, depth+1)
-      draw_tree(order-1, theta, newsz, newpos, heading+theta, color2, depth+1)
+        # make the recursive calls to draw the two subtrees
+        newsz = sz*(1 - trunk_ratio)
+        draw_tree(order-1, theta, newsz, newpos, heading-theta, color1, depth+1)
+        draw_tree(order-1, theta, newsz, newpos, heading+theta, color2, depth+1)
 
 
 def gameloop():
